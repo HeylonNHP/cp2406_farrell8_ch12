a0596e49527c01f5a60c9494e38ef34f9d4c2d29
HeylonNHP <heylon.white@my.jcu.edu.au>
2017-09-27 14:17:45 +1000

    done

--- DebuggingExercises/DebugTwelve4.java
+++ DebuggingExercises/DebugTwelve4.java
@@ -9,10 +9,10 @@ public class DebugTwelve4
       String inStr, outString = "";
       final int MAX = 999;
       int[] emp = new int[4];
-      for(x = 0; x < emp.length; ++x)
+      for(int x = 0; x < emp.length; ++x)
       {
          inStr = JOptionPane.showInputDialog(null, "Enter employee ID number");  
-         throw
+         try
          {
             emp[x] = Integer.parseInt(inStr);
             if(emp[x] > MAX)
@@ -25,10 +25,10 @@ public class DebugTwelve4
             --x;
             JOptionPane.showMessageDialog(null, inStr + "\nNonnumeric ID");
          }
-         catc(FixDebugEmployeeIDException error)
+         catch(FixDebugEmployeeIDException error)
          {	
 	    --x;
-            JOptionPane.showMmrge");
+            JOptionPane.showMessageDialog(null, error.getMessage());
          }
       }
       for(int x = 0; x < emp.length; ++x)
